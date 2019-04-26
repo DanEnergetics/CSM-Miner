fcbb22c63a1e34d2971a93ea2a1d2ba4cf7289dd
Odysseas <odysseas.karanikas@rwth-aachen.de>
2019-04-26 19:15:43 +0200

    no message

--- Source/Backend/CSMMiner/urls.py
+++ Source/Backend/CSMMiner/urls.py
@@ -13,21 +13,54 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import datetime
+import os
+from django.shortcuts import render
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from django import template
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader, Template, Context
+from django.views.decorators.csrf import csrf_protect
+from django import forms
+from django.core.files import File
 
+class UploadFileForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+register = template.Library()
 
 def file_get_contents(filename):
     with open(filename) as f:
         return f.read()
 		
 		
-def main(request):
-	return HttpResponse(file_get_contents("./CSMMiner/HTMLDocs/index.html"))
+	
+@csrf_protect
+def get(request):
+	if request.method == 'POST':
+		file = request.FILES['file']
+		filename = file.name
+		if not filename.endswith('.csm'):
+			context = {}
+			return render(request, os.path.join(BASE_DIR,'.\HTMLDocs\index.html'), context)
+		md = File(file)
+		with open(os.path.join(BASE_DIR,'./Storage/',filename),'wb+') as dest:
+			for chunk in md.chunks():
+				dest.write(chunk)
+	context = {}
+	return render(request, os.path.join(BASE_DIR,'.\HTMLDocs\index.html'), context)
 
+
+def mainHandle(request,string):
+	handleForm(request)
+	return main(request)
+	
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('', main),
+	path('', get),
 ]
