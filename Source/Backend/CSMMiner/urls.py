51ff2245cf3553de1203128411dc3ada5b4da9ef
Odysseas <odysseas.karanikas@rwth-aachen.de>
2019-04-26 14:52:17 +0200

    Django setup, Appdrawer first look

---
+++ Source/Backend/CSMMiner/urls.py
@@ -0,0 +1,33 @@
+"""CSMMiner URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+
+
+def file_get_contents(filename):
+    with open(filename) as f:
+        return f.read()
+		
+		
+def main(request):
+	return HttpResponse(file_get_contents("./CSMMiner/HTMLDocs/index.html"))
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+	path('', main),
+]
