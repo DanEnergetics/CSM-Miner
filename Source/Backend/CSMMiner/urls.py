8742d3a67ca3f3a73521c5dfef183187216c10ee
Odysseas Karanikas <odysseas.karanikas@rwth-aachen.de>
2019-05-07 17:23:16 +0200

    Frontend update

--- Source/Backend/CSMMiner/urls.py
+++ Source/Backend/CSMMiner/urls.py
@@ -28,6 +28,13 @@ from django import forms
 from django.core.files import File
 from django.http import HttpResponseNotFound
 from django.conf.urls import (handler400, handler403, handler404, handler500)
+from django import template
+from xml.dom import minidom
+import hashlib
+import json
+from shutil import copyfile
+
+register = template.Library()
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=50)
@@ -39,6 +46,23 @@ register = template.Library()
 def file_get_contents(filename):
     with open(filename) as f:
         return f.read()
+
+def createProject(name):
+	hashedName = hashlib.sha256(name.encode())
+	hexed = hashedName.hexdigest()
+	print(hexed)
+	dir = os.path.join(BASE_DIR,'./Storage/',hexed)
+	if not os.path.exists(dir):
+		os.makedirs(dir)
+		jsonContent = '{ "name":"' + name +'", "filename":"' + name + '", "creationTime":"' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'", "lastEdit":"' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'", "backgroundImgPath": "' + BASE_DIR +'/HTMLDocs/images/matrix.png", "preProcessed": "NO",}'
+		jsonConvert = json.dumps(jsonContent, indent=4, sort_keys=True)
+		print(dir)
+		fileW = open(os.path.join(dir ,"index.json"),"w")
+		fileW.write(jsonConvert)
+		fileW.close()
+		return dir
+	else:
+		return "EXISTS"
 
 def get_projects():
 	return {'project1','project2'}
@@ -63,30 +87,42 @@ def request(rq,action):
 	"""
 	return HttpResponse('<body>OK</body>')
 	
+@register.filter
 @csrf_protect
-def iframe(request,file):
+def iframe(request,fileName):
+	fileName += ".html"
 	if request.method == 'POST':
 		file = request.FILES.get('file',"EMPTY_REQ")
 		if file == "EMPTY_REQ":
 			context = {
 				'msg' : "No file detected.",
 			}
-			return render(request, os.path.join(BASE_DIR,"./HTMLDocs/",file,".html"), context)
+			return render(request, os.path.join(BASE_DIR,"HTMLDocs/",fileName), context)
 		file = request.FILES['file']
 		filename = file.name
 		if not filename.endswith('.xes'):
 			context = {
 				'msg' : "Wrong format detected.",
 			}
-			return render(request, os.path.join(BASE_DIR,"./HTMLDocs/",file,".html"), context)
+			return render(request, os.path.join(BASE_DIR,"HTMLDocs/",fileName), context)
 		md = File(file)
-		with open(os.path.join(BASE_DIR,'./Storage/',filename),'wb+') as dest:
-			for chunk in md.chunks():
-				dest.write(chunk)
+		DEST_DIR = createProject(filename)
+		if not DEST_DIR == "EXISTS":
+			context = {
+				'msg' : "FILE_OK",
+			}
+			with open(os.path.join(DEST_DIR,filename),'wb+') as dest:
+				for chunk in md.chunks():
+					dest.write(chunk)
+			return render(request, os.path.join(BASE_DIR,"HTMLDocs/",fileName), context)
+		context = {
+			'msg' : "Project already exists."
+		}
+		return render(request, os.path.join(BASE_DIR,"HTMLDocs/",fileName), context)
 	context = {
-		'msg' : "";
+		'msg' : "",
 	}
-	return render(request, os.path.join(BASE_DIR,"./HTMLDocs/",file,".html"), context)	
+	return render(request, os.path.join(BASE_DIR,"HTMLDocs/",fileName), context)	
 
 
 def mainHandle(request,string):
