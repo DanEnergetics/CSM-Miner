286a12f6cf3521349e1601130283609dcf8d2b14
Odysseas Karanikas <odysseas.karanikas@rwth-aachen.de>
2019-05-24 15:41:33 +0200

    See last

--- Source/Backend/View/ViewSet.py
+++ Source/Backend/View/ViewSet.py
@@ -18,6 +18,31 @@ class ViewSet:
     def getView(self,Pos):
         if len(self.views) > Pos:
             return self.views[Pos]
+    
+    def createViewsForLabelsWithLabels(_View = View(),_Map = {}):
+        labels = []
+        _ViewSet = ViewSet()
+        for a,b in _Map:
+            if not a in labels:
+                labels.append(a)
+        for i in range(len(labels)):
+            _ViewSet.addView(_View)
+        return [_ViewSet,labels]
+
+
+    def partition(_View,_Map):
+        main = createViewsForLabelsWithLabels(_View,_Map)
+        for i in range(len(main[1])):
+            for element in main[0].getView(i):
+                for a,b in element.getDirect():
+                    if _Map[a] != main[1][i] or _Map[b] != main[1][i] :
+                        del element.directSucc[a]
+                        if _Map[a] != main[1][i]:
+                            element.nodes.remove(a)
+
+
+
+
 
 class View:
     nodes = []
