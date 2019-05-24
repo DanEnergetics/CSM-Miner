8838bab24d74bd1a3468388d49833ceab466dc8e
Odysseas Karanikas <odysseas.karanikas@rwth-aachen.de>
2019-05-24 16:55:30 +0200

    FIX

---
+++ Source/Backend/View/ViewClass.py
@@ -0,0 +1,47 @@
+import json
+
+class View:
+    nodes = []
+    directSucc = None
+    indirectSucc = None
+
+    def __init__(self,_nodes = [],_directSucc= None,_indirectSucc = None):
+        self.directSucc = {} if _directSucc == None else _directSucc
+        self.nodes = _nodes
+        self.indirectSucc = {} if _indirectSucc == None else _indirectSucc
+    
+    def addNode(self,_Node):
+        nodes.append(_Node)
+
+    def addDirectSucc(self,sourceNode, targetNode,frequency):
+        sel.directSucc[sourceNode] = {}
+        self.directSucc[sourceNode][targetNode] = frequency
+    
+    def addIndirectSucc(self,sourceNode, targetNode,frequency):
+        self.indirectSucc[sourceNode] = {}
+        self.indirectSucc[sourceNode][targetNode] = frequency
+    
+
+    def getNodes(self):
+        return self.nodes
+    
+    def getIndir(self):
+        return self.indirectSucc
+
+    def getDirect(self):
+        return self.directSucc
+
+    def toJson(self):
+        main = []
+        main.append(self.nodes)
+        main.append(self.directSucc)
+        main.append(self.indirectSucc)
+        return json.dumps(main,indent=4)
+
+    def fromJson(jsonString):
+        main = json.loads(jsonString)
+        try:
+            rt = View(main[0],main[1],main[2])
+            return rt
+        except:    
+            return "Error"
--- Source/Backend/View/ViewSet.py
+++ Source/Backend/View/ViewSet.py
@@ -1,4 +1,5 @@
 import json
+from ViewClass import View
 
 class ViewSet:
     views = []
@@ -19,10 +20,14 @@ class ViewSet:
         if len(self.views) > Pos:
             return self.views[Pos]
     
-    def createViewsForLabelsWithLabels(_View = View(),_Map = {}):
+    def createViewsForLabelsWithLabels(_View = View(),_Map = None):
+        if _Map == None :
+           Map = {}
+        else :
+            Map = _Map
         labels = []
         _ViewSet = ViewSet()
-        for a,b in _Map:
+        for a,b in Map:
             if not a in labels:
                 labels.append(a)
         for i in range(len(labels)):
@@ -39,51 +44,3 @@ class ViewSet:
                         del element.directSucc[a]
                         if _Map[a] != main[1][i]:
                             element.nodes.remove(a)
-
-
-
-
-
-class View:
-    nodes = []
-    directSucc = {{}}
-    indirectSucc = {{}}
-
-    def __init__(self,_nodes = [],_directSucc= {{}},_indirectSucc = {{}}):
-        self.nodes = _nodes
-        self.directSucc = _directSucc
-        self.indirectSucc = _indirectSucc
-    
-    def addNode(self,_Node):
-        nodes.append(_Node)
-
-    def addDirectSucc(self,sourceNode, targetNode,frequency):
-        self.directSucc[sourceNode][targetNode] = frequency
-    
-    def addIndirectSucc(self,sourceNode, targetNode,frequency):
-        self.indirectSucc[sourceNode][targetNode] = frequency
-    
-
-    def getNodes(self):
-        return self.nodes
-    
-    def getIndir(self):
-        return self.indirectSucc
-
-    def getDirect(self):
-        return self.directSucc
-
-    def toJson(self):
-        main = []
-        main.append(self.nodes)
-        main.append(self.directSucc)
-        main.append(self.indirectSucc)
-        return json.dumps(main,indent=4,sort_keys=True)
-
-    def fromJson(jsonString):
-        main = json.loads(jsonString)
-        try:
-            rt = View(main[0],main[1],main[2])
-            return rt
-        except:    
-            return "Error"
